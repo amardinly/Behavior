804e8fa061b2826ce56e4c7bcbadf956c800636e
Alan Mardinly <alan.mardinly@gmail.com>
2018-06-25 16:22:31 -0700

    resolving conflicts again

--- HB_whisker_deflection_calibration/whisker_deflection_matlab_control.m
+++
@@ -1,7 +0,0 @@
-s=serial('COM7','BaudRate',9600);
-fopen(s);
-readData=fscanf(s) %reads "A"
-flushinput(s);fprintf(s,'%s\n','fff','sync');
-fprintf(s,'%s\n','1','sync');readData=fscanf(s)
-
-fclose(s);
