47f5bba043d737a4895eb0acc4b86772b99b5e97
hayleybounds <hayleybounds@gmail.com>
2019-07-07 17:14:36 -0700

    building up psychopy and daq

--- ContrastDetectionPsychoPy.py
+++ ContrastDetectionPsychoPy.py
@@ -1,7 +1,8 @@
 from psychopy import visual, core,monitors,data, event,gui# import some libraries from PsychoPy
 import numpy as np
 import datetime
-
+import random
+import nidaqmx as ni
 
 class ContrastDetectionTask:
     
@@ -14,9 +15,11 @@ class ContrastDetectionTask:
         if dlg.OK==False: core.quit()
 
 
-        filename = base_dir + expInfo['date']+'_' + expInfo['mouse'] + '.csv'
+        self.filename = base_dir + expInfo['date']+'_' + expInfo['mouse']
         
-        exp = data.ExperimentHandler('ContrastDetectionTask','v0',dataFileName = filename)
+        self.exp = data.ExperimentHandler('ContrastDetectionTask','v0',
+            dataFileName = self.filename,
+            extraInfo = expInfo)
 
         #stimulus variables
         tf = 2 #temporal frequency
@@ -31,59 +34,79 @@ class ContrastDetectionTask:
                86 , 100 ]
         
         #task variables
-        self.stim_delay = .1
-        self.stim_time = .6
-        self.response_window = 1
-        self.isimin = 1
-        self.isimax = 4
-        self.grace_time = 1
+        self.stim_delay = .1 #in s
+        self.stim_time = .6 #in s
+        self.response_window = 1 # in s
+        self.isimin = 3 # in s
+        self.isimax = 8 #in s
+        self.grace_time = 1 #in s
+        self.water_time = 50 #in ms
         tf=2
     
         #monitor variables
-        monitor_width = 25 #in cm
-        monitor_dist = 20 #in cm
+        monitor_width = 22 #in cm
+        monitor_dist = 10 #in cm
         
         #initialize 
         self.monitor = monitors.Monitor('testMonitor', width=monitor_width, distance=monitor_dist)
-        self.win = visual.Window(fullscr=False, monitor=self.monitor, units="deg")
+        self.win = visual.Window(fullscr=True, monitor=self.monitor, units="deg")
         FR = self.win.getActualFrameRate()
         expInfo['FR'] = FR        
         
         
         #create trial conditions
         self.size_int_response = []
-        for s in sizes:
-            for ins in intensities[s]:
-                self.size_int_response.append({'size':s,'intensity':ins,'corr_response':ins>0})
-        
+        for temp in range(10):
+            mini_size_int_response = []
+            for _ in range(4):
+                for s in sizes:
+                    for ins in intensities[s]:
+                        mini_size_int_response.append({'size':s,'intensity':ins,'corr_response':ins>0})
+            random.shuffle(mini_size_int_response)
+            self.size_int_response += mini_size_int_response
         #and some more general variables
         self.phase_increment = tf/FR
         self.stim_on_frames = int(self.stim_time*FR)
-        print(FR, self.stim_time, self.stim_on_frames)
+
         self.trial_timer = core.Clock()
         self.isi_timer = core.Clock()
         self.grating = visual.GratingStim(win=self.win, size=8, pos=position, sf=sf,units='deg')
         
+        #set up the nidaq
+        self.lick_daq = ni.Task()
+        self.lick_daq.di_channels.add_di_chan('Dev2/Port1/Line0')
+        self.lick_daq.start()
+
+        self.water_daq = ni.Task()
+        self.water_daq.do_channels.add_do_chan('Dev2/Port1/Line2')
         
         self.run_blocks()
         
     def run_blocks(self):
-        user_quit = False
+        user_quit = False 
         while not user_quit:
             print('makin trials')
             #do in blocks of randomized trials
-            self.trials = data.TrialHandler(self.size_int_response, 4, method='random')
+
+            self.trials = data.TrialHandler(self.size_int_response, 1, method='sequential')
             self.trials.data.addDataType('Response')
+            self.exp.addLoop(self.trials)
             for trial in self.trials:
-                print('is a trial')
                 if user_quit:
                     break
 
                 user_quit = self.run_trial(trial)
+                if not user_quit:
+                    user_quit, false_alarm_times = self.run_isi()
+                    self.trials.data.add('FATimes',false_alarm_times)
+                self.exp.nextEntry()
 
-                user_quit, false_alarm_times = self.run_isi()
-
-                self.trials.data.add('FATimes',false_alarm_times)
+            self.exp.saveAsWideText(self.filename)
+            self.trials.saveAsWideText(self.filename + '_trials')
+            self.water_daq.stop()
+            self.lick_daq.stop()
+            self.water_daq.close()
+            self.lick_daq.close()
 
     def present_grating(self):
         phase = 0
@@ -98,6 +121,7 @@ class ContrastDetectionTask:
         self.trial_timer.reset()
         responded = []
         response_time = np.nan
+        user_quit = False
         
         while trial_still_running:
 
@@ -111,8 +135,10 @@ class ContrastDetectionTask:
             if current_time <= self.stim_delay:
                 continue
 
-            elif current_time >= self.stim_delay and current_time <= self.stim_time+self.stim_delay-.1:
-                print(self.trials.thisN,  current_time)
+            elif (current_time >= self.stim_delay and 
+                  current_time <=
+                    self.stim_time+self.stim_delay-.1):
+                print(trial['intensity'], trial['size'])
                 self.grating.setContrast(trial['intensity']/100)
                 self.grating.setSize(trial['size'])
                 self.present_grating()
@@ -122,22 +148,23 @@ class ContrastDetectionTask:
                 self.grating.setContrast(0)
                 self.grating.draw()
 
-                responded = e
+                responded = self.lick_daq.read()
 
-            if responded == 1:
+
+            if responded:
                 if trial['corr_response']:
-                    deliver_reward(mywin)
+                    self.deliver_reward()
                 
                 response_time = current_time
-
+                print(response_time)
                 trial_still_running = False
-                trials.data.add('Response', 1)
-                trials.data.add('RespTime', response_time)
+                self.trials.data.add('Response', 1)
+                self.trials.data.add('RespTime', response_time)
 
             elif current_time >= 1.7:
 
                 trial_still_running = False
-                trials.data.add('Response', 0)
+                self.trials.data.add('Response', 0)
 
             self.win.flip()
         
@@ -145,8 +172,11 @@ class ContrastDetectionTask:
         return user_quit
 
     def deliver_reward(self):
-        print('REWARD')
-        pass
+        self.water_daq.start()
+        self.water_daq.write(True)
+        core.wait(self.water_time/1000)
+        self.water_daq.write(False)
+        self.water_daq.stop()
 
     def run_isi(self):
         user_quit = False
@@ -154,18 +184,21 @@ class ContrastDetectionTask:
 
         self.isi_timer.reset()
         false_alarm_times = []
-        while self.isi_timer.getTime() < isi/1000:
+        while self.isi_timer.getTime() < isi:
             e=event.getKeys()
             if len(e)>0:
                 if e[0]=='q':
                     user_quit=True
                     return user_quit, []
-            if self.isi_timer.getTime() > self.grace_time/1000: #if we're out of grace period
+            if self.isi_timer.getTime() > self.grace_time: #if we're out of grace period
                 #check for a false alarm
-                responded = e
+                responded = self.lick_daq.read()
                 if responded:
                     print('false alarm!')
                     false_alarm_times.append(round(self.trial_timer.getTime(),2))
                     isi = np.random.randint(self.isimin,self.isimax)
                     self.isi_timer.reset()
         return user_quit, false_alarm_times
+
+if __name__ == '__main__':
+    ContrastDetectionTask()
