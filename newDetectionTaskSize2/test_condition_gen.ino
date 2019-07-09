5ea23dcb6746291420f543e9ae6aed3ab6b63047
hayleybounds <hayleybounds@gmail.com>
2019-07-08 21:05:10 -0700

    fixed test conditions in C
    need to test+transfer to arduino

--- newDetectionTaskSize2/test_condition_gen.ino
+++ newDetectionTaskSize2/test_condition_gen.ino
@@ -1,5 +1,6 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int size1 = 20;
 int size2 = 50;
@@ -15,8 +16,18 @@ int stimVals[3500];
 int optoVals[3500];
 int ISIdistribution[3500];
 
-int trialNumber = 3000;
+int trialNumber = 2000;
+int state = 3;
+
+int rand_range(int max){
+    int x = rand() % (max + 1 - 0) + 0;
+    return x;
+}
+
+
+
 void chooseParams() {
+    
       
           int theLevels_s1[6]={0,4,16,64,180,255};
           int theWeights_s1[6] = {1,1, 1, 1, 1, 1};
@@ -28,50 +39,36 @@ void chooseParams() {
             conditions[index][0] = size1;
             conditions[index][1] = theLevels_s1[index];
             conditionWeights[index] = theWeights_s1[index];
+            
           }
           
-          
-          for (int index = (sizeof(theWeights_s1) / sizeof(int));
-            index < (sizeof(theWeights_s2) / sizeof(int)) + (sizeof(theWeights_s1) / sizeof(int));
+          int add_to_index = (sizeof(theWeights_s1) / sizeof(int));
+          for (int index = 0;
+            index < (sizeof(theWeights_s2) / sizeof(int));
             index++){
-                conditions[index][0] = size2;
-                conditions[index][1] = theLevels_s2[index];
-                conditionWeights[index] = theWeights_s2[index];
+                conditions[index+add_to_index][0] = size2;
+                conditions[index+add_to_index][1] = theLevels_s2[index];
+                conditionWeights[index+add_to_index] = theWeights_s2[index];
           }
+          
           
 }
 
-int get_n_conditions(){
+
+
+void test(){
     int output_size = 0;
     for (int index = 0; index < (sizeof(conditionWeights) / sizeof(int)); index++){
     if (conditionWeights[index] != 0){
       output_size++;
     }
   }
-  return output_size;
-}
 
-void bubbleUnsort(int *list, int elem){
-  for (int a = elem - 1; a > 0; a--){
-    int r = random(a+1);
-    if (r != a){
-      int temp = list[a];
-      list[a] = list[r];
-      list[r] = temp;
-    }
-  }
-}
-
-
-void populateTrials() {
-   //the number of weights actually being used
-  int output_size = get_n_conditions();
-  //the total size of all weights added together
   int arraySum = 0;
   for (int index = 0; index < output_size; index++){
     arraySum += conditionWeights[index];
   }
-  //populate an array containing all the necessary duplicatses...
+  
   int weightedOutputs[arraySum][2];
   int i = 0;
   //for each unique contrast and associated weight
@@ -83,10 +80,9 @@ void populateTrials() {
       i++;
     }
   }
-
   int arraySumTwo;
   if (doOpto==1){
-      int arraySumTwo = optoWeights[0]*arraySum + optoWeights[1]*arraySum;
+      arraySumTwo = optoWeights[0]*arraySum + optoWeights[1]*arraySum;
   }else{
       arraySumTwo = arraySum*2;
   }
@@ -94,17 +90,22 @@ void populateTrials() {
   int weightedOptoOutputs[arraySumTwo][3];
   int conditionIndexes[arraySumTwo];
   
+  
   if (doOpto==1){
       // then we'll duplicate that whole array, and give the firs thalf non opto weights
-      int i=0;
-      for (int n=0; n < 2; n++){
-          for (int k = 0; k < arraySum; k++){
-              for (int l=0; l < optoWeights[n]; l++){
-                  weightedOptoOutputs[i][0] = weightedOutputs[i][0];
-                  weightedOptoOutputs[i][1] = weightedOutputs[i][1];
-                  weightedOptoOutputs[i][2] = n;
-                  conditionIndexes[i] = i;
-                  i++;
+      int wCondInd=0;
+      for (int optoCond=0; optoCond < 2; optoCond++){ // each opto condition
+            // printf("(arraySum: %d)", arraySum);
+          for (int k = 0; k < arraySum; k++) { // for all weights + conditions
+              for (int optoW = 0; optoW < optoWeights[optoCond]; optoW++){
+                  weightedOptoOutputs[wCondInd][0] = weightedOutputs[k][0];
+                  weightedOptoOutputs[wCondInd][1] = weightedOutputs[k][1];
+                  weightedOptoOutputs[wCondInd][2] = optoCond;
+                  conditionIndexes[wCondInd] = wCondInd;
+                   printf("%d & %d, ", weightedOutputs[k][1], optoCond);
+                  
+                  wCondInd++;
+                  
               }
             }
           }
@@ -121,31 +122,29 @@ void populateTrials() {
       }
   }  
   
-
-
-    
   int k = 0;
   int arrLen = sizeof(conditionIndexes) / sizeof(int);
-  int rand_n;
-  while (k < trialNumber) {
-    bubbleUnsort(conditionIndexes, arrLen);       //randomize the weighted trial lengths
 
+  while (k < trialNumber) {
+      
+      
+    for (int a = arrLen - 1; a > 0; a--){
+        int r = rand_range(a);
+        if (r != a){
+          int temp = conditionIndexes[a];
+         conditionIndexes[a] = conditionIndexes[r];
+          conditionIndexes[r] = temp;
+       }
+    }
     for (int n = 0; n < (sizeof(conditionIndexes) / sizeof(int)); n++) {
       int condInd = conditionIndexes[n];
       stimVals[k] = weightedOptoOutputs[condInd][0];
       sizeVals[k] = weightedOptoOutputs[condInd][1];
       optoVals[k] = weightedOptoOutputs[condInd][2];
+    
       
+      printf("(%d, %d, %d)", stimVals[k],sizeVals[k],optoVals[k]);
       k++;
     }
   }
-}//END POPULATE TRIALS
-
-
-
-void main()
-{
-    chooseParams();
-    populateTrials();
-    printf("its %d", optoVals[1]);
 }
